cc4346de750d87c0ce76b23d8739aa86eb1a8fe7
LuisFerOv25 <ingluisfer25@udenar.edu.co>
2022-09-19 16:37:20 -0500

    Funcion visualizar empleado realizada

--- js/back-end.js
+++ js/back-end.js
@@ -1,125 +1,116 @@
 class Empleado {
-    constructor(Nombre, Apellido, Genero, FechaNac, FechaIngr, Foto) {
+    constructor(Nombre, Apellido, Genero, FechaNac, FechaIngr, Foto, Salario) {
         this.Nombre = Nombre;
         this.Apellido = Apellido;
         this.Genero = Genero;
         this.FechaNac = FechaNac;
         this.FechaIngr = FechaIngr;
         this.Foto = Foto;
+        this.Salario = Salario;
     }
-    toStringNombre(){
-        return this.Nombre;
+    toStringNombre() {
+        return (this.Nombre = document.getElementById("nombre").value);
     }
-    toStringApellido(){
-        return this.Apellido;
+    toStringApellido() {
+        return (this.Apellido = document.getElementById("apellido").value);
     }
-    toStringGenero(){
-        return this.Genero;
+    toStringGenero() {
+        return (this.Genero = document.querySelector(
+            'input[name="gen"]:checked'
+        ).value);
     }
-    
-    visualizarInfoEmp(nom) {
-        this.Nombre
-        // return (
-        //     "Nombre: " +
-        //     this.Nombre +
-        //     "\n" +
-        //     "Apellido" +
-        //     this.Apellido +
-        //     "\n" +
-        //     "Genero" +
-        //     this.Genero +
-        //     "\n"
-        // );
-        // "FechaNac" + this.FechaNac + "\n";
-        // "FechaIngr" + this.FechaIngr + "\n";
-        // "Foto" + this.Foto + "\n";
+    toStringFechaNac() {
+        return (this.FechaNac = document.getElementById("fechaNac").value);
     }
-    actualizarSalario() {
-        let Salario = parseFloat(document.getElementById("salario").value);
-        return Salario;
+    toStringFechaIng() {
+        return (this.FechaIngr = document.getElementById("fIngreso").value);
     }
-    calcularEdad() {
+    toStringSalario() {
+        return (this.Salario = parseFloat(document.getElementById("salario").value));
+    }
+    toStringFoto() {
+        return (this.Foto = document.getElementById("imagen").files[0]);
+    }
 
+  
+
+    calcularEdad() {
         let Calcular_Edad = document.getElementById("fechaNac").value;
         let anio = parseInt(Calcular_Edad.substr(6, 4));
-        let eda = 2022-anio;
+        let eda = 2022 - anio;
     }
     calcularAntig() {
         let Fecha_Ingreso = document.getElementById("fIngreso").value;
         let ingreso = parseInt(Fecha_Ingreso.substr(6, 4));
-        let antiguedad = 2022-ingreso;
+        let antiguedad = 2022 - ingreso;
         return antiguedad;
     }
     calcularPrestac() {
-
         let anti = new this.calcularAntig();
         let sal = new this.actualizarSalario();
-        let prestaciones = (anti*sal)/12;
+        let prestaciones = (anti * sal) / 12;
         return prestaciones;
     }
 }
-// function alerta(){
-//     alert('H
-// function calcularEdad() {
 
 
-//     alert(calcularPrestac());
-
-// }
 let verinfo = new Empleado();
 
-function mostrar_datos(){
-    let nombreN = document.getElementById("nombre").value;
-    
-    //let apellido = document.getElementById("apellido").value;
-    let nom = verinfo.toStringNombre(nombreN);
-    alert(nom);
-    //document.getElementById('visnombre').innerHTML = nom;
+function mostrar_datos() {
+    var reader = new FileReader();
+    if (imagen) {
+      reader.readAsDataURL(verinfo.toStringFoto());
+      reader.onloadend = function () {
+        document.getElementById("foto").src = reader.result;
+      }
+    }
+    document.getElementById("nomMos").innerHTML = verinfo.toStringNombre();
+    document.getElementById("apeMos").innerHTML = verinfo.toStringApellido();
+    document.getElementById("genMost").innerHTML = verinfo.toStringGenero();
+    document.getElementById("fechaNacMos").innerHTML = verinfo.toStringFechaNac();
+    document.getElementById("fechaIngMos").innerHTML = verinfo.toStringFechaIng();
+    document.getElementById("salMos").innerHTML = verinfo.toStringSalario();
 }
 
-class seguridad_social{
-
-   
-    constructor(salud,pension,arl,total){
+class seguridad_social {
+    constructor(salud, pension, arl, total) {
         this.salud = 0.125;
         this.pension = 0.16;
         this.arl = arl;
-        this.total =total;
-        
+        this.total = total;
     }
 
-    calcularSalud(){
+    calcularSalud() {
         var obj = new Empleado();
         let salar = obj.actualizarSalario();
-        let resS = this.salud*salar;
+        let resS = this.salud * salar;
         return resS;
     }
-    calcularPension(){
+    calcularPension() {
         var obj = new Empleado();
         let pens = obj.actualizarSalario();
-        let resP = this.pension*pens;
+        let resP = this.pension * pens;
         return resP;
     }
-    calcularArl(){
+    calcularArl() {
         var obj = new Empleado();
         this.arl = parseFloat(document.getElementById("nivelR").value);
         let SalArl = obj.actualizarSalario();
-        let resA = this.arl*SalArl;
+        let resA = this.arl * SalArl;
         return resA;
     }
-    totalPago(){
-        this.total = this.calcularSalud()+this.calcularPension() + this.calcularArl();
+    totalPago() {
+        this.total =
+            this.calcularSalud() + this.calcularPension() + this.calcularArl();
         return this.total;
     }
-
-    
 }
 
 let resul = new seguridad_social();
-function mos(){
-
-    document.getElementById('salud').innerHTML = resul.calcularSalud();
-    document.getElementById('pension').innerHTML = resul.calcularPension();
-    document.getElementById('arl').innerHTML = resul.calcularArl();
-    document.getElementById('total').innerHTML = resul.totalPago();
+function mos() {
+    document.getElementById("salud").innerHTML = resul.calcularSalud();
+    document.getElementById("pension").innerHTML = resul.calcularPension();
+    document.getElementById("arl").innerHTML = resul.calcularArl();
+    document.getElementById("total").innerHTML = resul.totalPago();
 }
+
